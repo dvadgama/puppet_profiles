2479ac9f720518c6fa6d2b4f674ca2842de9bc2c
Dhaval Vadgama <daval.it@gmail.com>
2015-03-22 21:41:40 +0000

    added complete test for RedHat osfamily

--- spec/classes/profiles_puppet_master_spec.rb
+++ spec/classes/profiles_puppet_master_spec.rb
@@ -7,19 +7,41 @@ describe 'profiles::puppet::master' do
  context 'Redhat/centos based system' do
     let :facts do
     {
-      :osfamily => 'RedHat'
+      :osfamily       => 'RedHat',
+      :environment    => 'test',
+      :ipaddress_eth0 => '10.10.10.10',
     }
     end
     
     context 'check the class inclusion' do
-      it { should contain_class('profiles::puppet::master') }
-      it { should contain_class('custom::puppet::install')  }
-      it { should contain_class('custom::puppet::config')   }
-      it { should contain_class('custom::puppet::service')  }
-    end
+      it { should contain_class('custom::puppet::host').with_hostdata({ `hostname`.strip.downcase  => {"ip" => "10.10.10.10",
+                                                                                                       "target" => "/etc/hosts",
+                                                                                                       "host_aliases" => ["puppet","puppetmaster",`hostname`.strip.downcase], 
+                                                                                                      } 
+                                                                       }) 
+         }
 
-    context 'check the hiera data' do
-   
+      it { should contain_class('custom::puppet::install').with_packages("puppet-server"=>{"ensure"=>"installed"}) }
+
+      it { should contain_class('custom::puppet::config')   }
+
+      it { should contain_class('custom::puppet::service')  }
+
+      it { should contain_class('profiles::puppet::master') }
+
+      it { should contain_file('/etc/puppet/puppet.conf') }
+    
+      it { should contain_file('/etc/puppet/environments/test/environment.conf') }
+
+      it { should contain_host(`hostname`.strip.downcase) }
+
+      it { should contain_package('puppet-server') }
+ 
+      it { should contain_service('puppetmaster') }
+
+      it { should contain_custom__puppet__template('/etc/puppet/environments/test/environment.conf') }
+
+      it { should contain_custom__puppet__template('/etc/puppet/puppet.conf') }
     end
 
   end
