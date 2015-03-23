454eaaf172526aa61319a8b0928f5106c9e23428
Dhaval Vadgama <daval.it@gmail.com>
2015-03-23 17:27:02 +0000

    updated spec_helper with RSpec config to display coloured output in doc format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'hiera-puppet-helper'
 
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
+
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 shared_context "hieradata" do
