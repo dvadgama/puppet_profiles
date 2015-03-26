f8c2103500c74937338ccd650da1c3bd47e4af8c
Dhaval Vadgama <daval.it@gmail.com>
2015-03-26 09:06:45 +0000

    moving toward the shell() approch

--- spec/acceptance/foss_puppet_master_spec.rb
+++ spec/acceptance/foss_puppet_master_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper_acceptance'
 describe '::profiles::puppet::hiera' do
 
   it 'should apply the hiera config' do
-    pp = <<-EOS 
-           include ::profiles::puppet::hiera
-         EOS
-    apply_manifest(pp,:catch_failures => true)
-    expect(apply_manifest(pp,:catch_failures => true).exit_code).to be_zero
+    expect(shell('puppet apply --environment=master -e "include ::profiles::puppet::hiera').exit_code).to be_zero
+    
+    #pp = <<-EOS 
+    #       include ::profiles::puppet::hiera
+    #     EOS
+
+    #apply_manifest(pp,:catch_failures => true)
+    #expect(apply_manifest(pp,:catch_failures => true).exit_code).to be_zero
   end
 
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -15,29 +15,10 @@ RSpec.configure do | config |
   config.before :suite do
     hosts.each do | host |
       if host['roles'].include?('foss_master_from_scretch')
-        master = host
-        config_file = {
-        'main' => {
-          'server'   => "#{master}.example.com",
-          'certname' => "#{master}.example.com",
-          'logdir'   => '/var/log/puppet',
-          'vardir'   => '/var/lib/puppet',
-          'ssldir'   => '/var/lib/puppet/ssl',
-          'rundir'   => '/var/run/puppet',
-          'environment' => 'master',
-          'environmentpath' => '$confdir/environments',
-        },
-        'agent' => {
-          'environment' => 'master',
-        }
-      }
-
-      configure_puppet(host,config_file)
-      on host,"service puppet restart"    
-      scp_to host, "#{module_path}/spec/puppetmaster.sh",'/tmp/'
-      on host,"/tmp/puppetmaster.sh"
-
+        scp_to host, "#{module_path}/spec/puppetmaster.sh",'/tmp/'
+        on host,"/tmp/puppetmaster.sh"
       end #if
     end #host.ech
   end #config.before
+
 end # rspec.configure
